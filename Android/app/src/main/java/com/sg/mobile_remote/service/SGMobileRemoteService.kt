656986e1f9aaed0a6f5e9d71ec9fa0a30f24d6ed
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2021-02-16 23:07:55 +0300

    Fixed #7 Create MobileRemoteService

---
+++ Android/app/src/main/java/com/sg/mobile_remote/service/SGMobileRemoteService.kt
@@ -0,0 +1,14 @@
+package com.sg.mobile_remote.service
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class SGMobileRemoteService() : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+    }
+}
